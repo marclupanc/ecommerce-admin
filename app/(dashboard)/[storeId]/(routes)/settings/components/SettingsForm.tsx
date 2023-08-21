ccf00eaa5cc18cd3d9323764d9ac712f6b98100b
Marc <marclupanc@gmail.com>
2023-08-21 16:51:59 +0300

    Store settings page. Store name input. Buttons

---
+++ app/(dashboard)/[storeId]/(routes)/settings/components/SettingsForm.tsx
@@ -0,0 +1,92 @@
+"use client";
+import React, { useState } from "react";
+import * as z from "zod";
+import { Store } from "@prisma/client";
+import Heading from "@/components/ui/heading";
+import { Button } from "@/components/ui/button";
+import { LuTrash } from "react-icons/lu";
+import { Separator } from "@/components/ui/separator";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+interface SettingsForm {
+  initialData: Store;
+}
+
+const formSchema = z.object({
+  name: z.string().min(1),
+});
+
+type SettingsFormValues = z.infer<typeof formSchema>;
+
+const SettingsForm: React.FC<SettingsForm> = ({ initialData }) => {
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const form = useForm<SettingsFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  });
+
+  const onSubmit = async (data: SettingsFormValues) => {
+    console.log(data);
+  };
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading title="Settings" description="Manage store settings" />
+        <Button
+          variant="destructive"
+          size="sm"
+          onClick={() => setOpen(true)}
+          disabled={loading}
+        >
+          <LuTrash className="h-4 w-4" />
+        </Button>
+      </div>
+      <Separator />
+
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Store Name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} className="ml-auto" type="submit">
+            Save changes
+          </Button>
+        </form>
+      </Form>
+    </>
+  );
+};
+
+export default SettingsForm;
